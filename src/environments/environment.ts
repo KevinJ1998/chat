9f03fc3f2df0d6dcc57eeeba7239b1a4cc8ada92
Jonathan Israel Vásquez Vivas <jon2013.p@hotmail.com>
2020-12-09 09:02:27 -0500

    Proyecto Base

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyA6hHUudpUd2Zv6gnEVUX0rD7gE1FCKU8o",
+    authDomain: "crud2020b-37d59.firebaseapp.com",
+    projectId: "crud2020b-37d59",
+    storageBucket: "crud2020b-37d59.appspot.com",
+    messagingSenderId: "28386433769",
+    appId: "1:28386433769:web:fec1ab858b2f6d6eefae78",
+    measurementId: "G-Z498QZCF23"
+  }
 };
 
 /*
